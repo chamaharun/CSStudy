656349468b9c25f9811da96283a1826fe0ebb676
Takumi Kobayashi <TakumiKobayashi@TAKUMI-no-MacBook-Pro.local>
2015-07-01 10:00:47 +0900

    modified

--- MorphAnaly.c
+++ MorphAnaly.c
@@ -54,98 +54,34 @@ DictTable *DictTableAlloc(void)
     }
     table->node_num = 0;
     
-    return (list);
+    return (table);
 }
 
 
 /*------------------------*/
-/* 連結リストへのデータ追加関数 */
+/* データ追加関数 */
 /*------------------------*/
 DictNode *DictTableDataAdd(DictTable *table, int x)
 {
-    DictNode *ptr; /* 注目するノードへのポインタ */
-    DictNode *prev; /* 直前ノードへのポインタ */
-    DictNode *new_node;
+DictNode* new_node;
 
-    ptr = list->head;
-    prev = NULL;
+int n = table->node_num //nはすでに格納された数/配列の格納先
 
-    while (ptr) { /* 終端ノードに到達するまでループ */
-	if (ptr->data < x) {
-	    prev = ptr; /* 直前ノードの更新 */
-	    ptr = ptr->next; /* 注目ノードの更新 */
-	} else if (ptr->data == x) { /* x は登録済み */
-	    return (NULL);
-	} else { /* x を注目ノードの直前に追加 */
-	    new_node = DictNodeAlloc();
-	    if (new_node == NULL) { /* 領域確保失敗 */
-		exit (0); /* 終了 */
-	    }
-	    new_node->data = x;
-	    new_node->next = ptr; /* ポインタの付け替え(注目ノードの直前) */
-	    if (prev != NULL) { /* 連結リストの先頭以降に追加 */
-		prev->next = new_node; /* ポインタの付け替え */
-	    } else { /* 連結リストの先頭に追加 */
-		list->head = new_node;
-	    }
-	    list->node_num++; /* ノード総数の更新 */
-	    return (new_node);
-	}
-    }
-    /* 終端ノードに到達 */
-    /* x を終端に追加 */
-    new_node = DictNodeAlloc();
-    if (new_node == NULL) { /* 領域確保失敗 */
-	exit (0); /* 終了 */
-    }
-    new_node->data = x;
-    new_node->next = NULL; /* new_node は新たな終端ノード */
-    if (prev != NULL) { /* list は少なくともひとつのノードを有している */
-	prev->next = new_node; /* 更新前の終端ノードの直後が new_node となる */
-    } else { /* list は空(ノードがひとつも含まれない) */
-	list->head = new_node;
-    }
-    list->node_num++; /* ノード総数の更新 */
-    return (new_node);
+new_node = LinkedListNodeAlloc();
+        if (new_node == NULL) { /* 領域確保失敗 */
+        exit (0); /* 終了 */
+        }
+table->nodes[n] = new_node;
+table->nodes++;
+
+return new_node;
+
 }
 
-
-/*--------------------------*/
-/* 連結リストからのデータ削除関数 */
-/*--------------------------*/
-int DictTableDataDel(DictTable *table, int x)
-{
-    DictNode *ptr; /* 注目するノードへのポインタ */
-    DictNode *prev; /* 直前ノードへのポインタ */
-
-    ptr = list->head;
-    prev = NULL;
-
-    while (ptr) { /* 終端ノードに到達するまでループ */
-	if (ptr->data < x) {
-	    prev = ptr;
-	    ptr = ptr->next;
-	} else if (ptr->data == x) { /* ptr は削除ノード */
-	    if (prev == NULL) { /* ptr は先頭ノード */
-		list->head = ptr->next;
-	    } else { /* ptr は先頭ノードでない */
-		prev->next = ptr->next;
-	    }
-	    list->node_num--;
-	    free(ptr);
-	    return (1);
-	} else { /* 削除対象ノードはなし */
-	    return (0);
-	}
-    }
-    /* 終端ノードに到達 */
-    /* 削除対象ノードはなし */
-    return (0);
-}
 
 
 /*----------------*/
-/* 連結リスト作成関数 */
+/* テーブル作成関数 */
 /*----------------*/
 DictTable *DictTableMake(char *filename)
 {
@@ -173,24 +109,6 @@ DictTable *DictTableMake(char *filename)
     return (list);
 }
 
-
-/*---------------------*/
-/* 連結リストの領域開放関数 */
-/*---------------------*/
-void DictTableFree(DictTable *table)
-{
-    DictNode *ptr; /* 注目ノードへのポインタ */
-    DictNode *rem; /* 削除ノード */
-
-    ptr = list->head;
-
-    while (ptr) { /* 終端ノードに到達するまでループ */
-	rem = ptr;
-	ptr = ptr->next;
-	free(rem);
-    }
-    free(list);
-}
 
 
 /*------------------*/
